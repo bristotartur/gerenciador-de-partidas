43462d98e134c5302d08e6761e9db904bb7318f1
bristotartur <arturdarosabristot@gmail.com>
2023-12-21 13:42:37 -0300

    Interface de mapeamento GoalMapper adicionada

--- src/main/java/com/bristotartur/gerenciadordepartidas/mappers/GoalMapper.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/mappers/GoalMapper.java
@@ -1,4 +1,18 @@
 package com.bristotartur.gerenciadordepartidas.mappers;
 
+import com.bristotartur.gerenciadordepartidas.domain.match.specifications.Goal;
+import com.bristotartur.gerenciadordepartidas.dtos.GoalDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface GoalMapper {
+
+    GoalMapper INSTANCE = Mappers.getMapper(GoalMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Goal toNewGoal(GoalDto goalDto);
+
+    Goal toExistingGoal(Long id, GoalDto goalDto);
 }

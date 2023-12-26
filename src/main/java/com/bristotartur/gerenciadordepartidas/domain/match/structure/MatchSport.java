a87d3011741bd6da2a25ed8e847afc5e772eec3e
bristotartur <arturdarosabristot@gmail.com>
2023-12-26 20:07:52 -0300

    Modificações na estutura de MatchSport

--- src/main/java/com/bristotartur/gerenciadordepartidas/domain/match/structure/MatchSport.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/domain/match/structure/MatchSport.java
@@ -1,11 +1,13 @@
 package com.bristotartur.gerenciadordepartidas.domain.match.structure;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class MatchSport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
 }

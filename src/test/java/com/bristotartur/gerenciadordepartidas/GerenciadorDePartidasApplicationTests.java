f7ca90731cffd2c64e678a0ab069f6bf77c0cb07
bristotartur <arturdarosabristot@gmail.com>
2023-12-30 12:01:36 -0300

    Modificações em GerenciadorDePartidasApplicationTests.java

--- src/test/java/com/bristotartur/gerenciadordepartidas/GerenciadorDePartidasApplicationTests.java
+++ src/test/java/com/bristotartur/gerenciadordepartidas/GerenciadorDePartidasApplicationTests.java
@@ -1,13 +1,24 @@
 package com.bristotartur.gerenciadordepartidas;
 
+import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class GerenciadorDePartidasApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertThat(context).isNotNull();
 	}
 
 }

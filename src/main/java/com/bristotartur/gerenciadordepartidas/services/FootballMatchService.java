d77349dac862b6f793dd3a13b85435ec1ee314dd
bristotartur <arturdarosabristot@gmail.com>
2024-01-04 11:19:06 -0300

    Documentação para implementações de MatchSportStrategy.java adicionada

--- src/main/java/com/bristotartur/gerenciadordepartidas/services/FootballMatchService.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/services/FootballMatchService.java
@@ -7,12 +7,24 @@ import com.bristotartur.gerenciadordepartidas.repositories.FootballMatchReposito
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Serviço responsável por gerenciar operações relacionadas a partidas de futebol (FootballMatch).
+ * Esta classe implementa a estratégia MatchSportStrategy para fornecer comportamentos padronizados
+ * relacionados à especialização de FootballMatch.
+ */
 @Service
 @AllArgsConstructor
 public class FootballMatchService implements MatchSportStrategy<FootballMatch> {
 
     private final FootballMatchRepository footballMatchRepository;
 
+    /**
+     * Busca uma partida de futebol pelo seu ID.
+     *
+     * @param id Identificador único da partida de futebol.
+     * @return Uma instância de FootballMatch correspondente ao ID fornecido.
+     * @throws NotFoundException Se nenhuma partida de futebol correspondente ao ID for encontrada.
+     */
     @Override
     public FootballMatch findMatchSportById(Long id) {
 
@@ -20,6 +32,11 @@ public class FootballMatchService implements MatchSportStrategy<FootballMatch> {
                 .orElseThrow(() -> new NotFoundException(ExceptionMessages.FOOTBALL_MATCH_NOT_FOUND.message));
     }
 
+    /**
+     * Cria uma nova instância de FootballMatch e a persiste no banco de dados.
+     *
+     * @return Uma nova instância de FootballMatch criada e salva no banco de dados.
+     */
     @Override
     public FootballMatch createNewMatchSport() {
         return footballMatchRepository.save(new FootballMatch());
--- src/main/java/com/bristotartur/gerenciadordepartidas/services/TableTennisMatchService.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/services/TableTennisMatchService.java
@@ -7,12 +7,24 @@ import com.bristotartur.gerenciadordepartidas.repositories.TableTennisMatchRepos
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Serviço responsável por gerenciar operações relacionadas a partidas de tênis de mesa (TableTennisMatch).
+ * Esta classe implementa a estratégia MatchSportStrategy para fornecer comportamentos padronizados
+ * relacionados à especialização de TableTennisMatch.
+ */
 @Service
 @AllArgsConstructor
 public class TableTennisMatchService implements MatchSportStrategy<TableTennisMatch> {
 
     private final TableTennisMatchRepository tableTennisMatchRepository;
 
+    /**
+     * Busca uma partida de tênis de mesa pelo seu ID.
+     *
+     * @param id Identificador único da partida de tênis de mesa.
+     * @return Uma instância de TableTennisMatch correspondente ao ID fornecido.
+     * @throws NotFoundException Se nenhuma partida de tênis de mesa correspondente ao ID for encontrada.
+     */
     @Override
     public TableTennisMatch findMatchSportById(Long id) {
 
@@ -21,6 +33,11 @@ public class TableTennisMatchService implements MatchSportStrategy<TableTennisMa
 
     }
 
+    /**
+     * Cria uma nova instância de TableTennisMatch e a persiste no banco de dados.
+     *
+     * @return Uma nova instância de TableTennisMatch criada e salva no banco de dados.
+     */
     @Override
     public TableTennisMatch createNewMatchSport() {
         return tableTennisMatchRepository.save(new TableTennisMatch());

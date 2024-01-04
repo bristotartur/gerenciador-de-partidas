62edcd79d9c5dfcf203065a4322a3d4c939e7f90
bristotartur <arturdarosabristot@gmail.com>
2024-01-04 12:32:25 -0300

    Documentação para TeamService.java adicionada

--- src/main/java/com/bristotartur/gerenciadordepartidas/services/TeamService.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/services/TeamService.java
@@ -12,6 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * A classe TeamService fornece serviços relacionados a operações CRUD para a entidade Team, interagindo
+ * com o repositório TeamRepository para acessar e manipular os dados relacionados a equipes.
+ */
 @Service
 @AllArgsConstructor
 public class TeamService {
@@ -19,10 +23,22 @@ public class TeamService {
     private final TeamMapper teamMapper;
     private final TeamRepository teamRepository;
 
+    /**
+     * Recupera todas as equipes disponíveis no sistema.
+     *
+     * @return Uma lista contendo todas as equipes.
+     */
     public List<Team> findAllTeams() {
         return teamRepository.findAll();
     }
 
+    /**
+     * Busca uma equipe específica com base em seu ID.
+     *
+     * @param id Identificador único da equipe.
+     * @return A equipe correspondente ao ID fornecido.
+     * @throws NotFoundException Se nenhuma equipe correspondente ao ID fornecido for encontrada.
+     */
     public Team findTeamById( Long id) {
 
         var team = teamRepository.findById(id)
@@ -31,16 +47,36 @@ public class TeamService {
         return team;
     }
 
+    /**
+     * Salva uma nova equipe no sistema com base nos dados fornecidos em TeamDto.
+     *
+     * @param teamDto Dados da equipe a serem salvos.
+     * @return A equipe recém-salva.
+     */
     public Team saveTeam(TeamDto teamDto) {
 
         var savedTeam = teamRepository.save(teamMapper.toNewTeam(teamDto));
         return savedTeam;
     }
 
+    /**
+     * Remove uma equipe do sistema com base em seu ID.
+     *
+     * @param id Identificador único da equipe a ser removida.
+     */
     public void deleteTeamById(Long id) {
         teamRepository.deleteById(id);
     }
 
+    /**
+     * Atualiza uma equipe existente no sistema com base em seu ID e os dados fornecidos em TeamDto.
+     * Isso envolve a substituição completa dos dados da equipe existente pelos novos dados fornecidos.
+     *
+     * @param id Identificador único da equipe a ser atualizada.
+     * @param teamDto Dados atualizados da equipe.
+     * @return A equipe atualizada.
+     * @throws NotFoundException Se nenhuma equipe correspondente ao ID fornecido for encontrada.
+     */
     @Transactional
     public Team replaceTeam(Long id, TeamDto teamDto) {
 

d03a1d55ecf4ef942e752f95a926a404b0aac576
bristotartur <arturdarosabristot@gmail.com>
2024-01-10 14:48:24 -0300

    Builder adicionado em MatchDto.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/MatchDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/MatchDto.java
@@ -4,9 +4,11 @@ import com.bristotartur.gerenciadordepartidas.enums.MatchStatus;
 import com.bristotartur.gerenciadordepartidas.enums.Modality;
 import com.bristotartur.gerenciadordepartidas.enums.Sports;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record MatchDto(@NotNull Sports sport,
                        @NotNull Long teamAId,
                        @NotNull Long teamBId,

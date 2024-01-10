663e6a4e925526c252eec153175b1ff005888241
bristotartur <arturdarosabristot@gmail.com>
2024-01-10 12:31:39 -0300

    Métodos CRUD adicionados em MatchService.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/services/MatchService.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/services/MatchService.java
@@ -1,16 +1,56 @@
 package com.bristotartur.gerenciadordepartidas.services;
 
+import com.bristotartur.gerenciadordepartidas.domain.match.structure.Match;
+import com.bristotartur.gerenciadordepartidas.dtos.MatchDto;
+import com.bristotartur.gerenciadordepartidas.enums.ExceptionMessages;
+import com.bristotartur.gerenciadordepartidas.exceptions.NotFoundException;
 import com.bristotartur.gerenciadordepartidas.mappers.MatchMapper;
 import com.bristotartur.gerenciadordepartidas.repositories.MatchRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Transactional
 public class MatchService {
 
     private final MatchRepository matchRepository;
     private final MatchMapper matchMapper;
     private final GeneralMatchSportService matchSportService;
+
+    public List<Match> findAllMatches() {
+        return matchRepository.findAll();
+    }
+
+    public Match findMatchById(Long id) {
+
+        var match = matchRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(ExceptionMessages.MATCH_NOT_FOUND.message));
+
+        return match;
+    }
+
+    public Match saveMatch(MatchDto matchDto) {
+
+        var savedMatch = matchMapper.toNewMatch(matchDto);
+        return savedMatch;
+    }
+
+    public void deleteMatchById(Long id) {
+        matchRepository.deleteById(id);
+    }
+
+    public Match replaceMatch(Long id, MatchDto matchDto) {
+
+        this.findMatchById(id);
+
+        var match = matchMapper.toExistingMatch(id, matchDto);
+        var updatedMatch = matchRepository.save(match);
+
+        return updatedMatch;
+    }
 
 }

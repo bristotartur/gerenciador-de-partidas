69e82a8988d00672bd1cf814b92d84041d831eea
bristotartur <arturdarosabristot@gmail.com>
2024-01-10 16:36:47 -0300

    Documentação para MatchService.java adicionada

--- src/main/java/com/bristotartur/gerenciadordepartidas/services/MatchService.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/services/MatchService.java
@@ -12,6 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Classe responsável por fornecer serviços relacionados a operações CRUD para a entidade Match,
+ * interagindo com o repositório MatchRepository para acessar e manipular dados relacionados a partidas.
+ */
 @Service
 @AllArgsConstructor
 @Transactional
@@ -21,10 +25,22 @@ public class MatchService {
     private final MatchMapper matchMapper;
     private final GeneralMatchSportService matchSportService;
 
+    /**
+     * Retorna todas as partidas disponíveis no banco de dados.
+     *
+     * @return Uma lista contendo todas as partidas.
+     */
     public List<Match> findAllMatches() {
         return matchRepository.findAll();
     }
 
+    /**
+     * Busca por uma entidade específica do tipo Match com base no seu ID.
+     *
+     * @param id Identificador único da partida.
+     * @return A partida correspondente ao ID fornecido.
+     * @throws NotFoundException Caso nenhuma partida correspondente ao ID for encontrada.
+     */
     public Match findMatchById(Long id) {
 
         var match = matchRepository.findById(id)
@@ -33,16 +49,36 @@ public class MatchService {
         return match;
     }
 
+    /**
+     * Salva uma partida no sistema com base nos dados fornecidos em MatchDto.
+     *
+     * @param matchDto Dados da partida a ser salva.
+     * @return A partida recém-salva.
+     */
     public Match saveMatch(MatchDto matchDto) {
 
         var savedMatch = matchRepository.save(matchMapper.toNewMatch(matchDto));
         return savedMatch;
     }
 
+    /**
+     * Remove uma partida do banco de dados com base no seu ID.
+     *
+     * @param id Identificador único da partida.
+     */
     public void deleteMatchById(Long id) {
         matchRepository.deleteById(id);
     }
 
+    /**
+     * Atualiza uma partida existente no banco de dados com base no seu ID e os dados fornecidos em MatchDto.
+     * Isso envolve a substituição completa dos dados da partida existente pelos novos dados fornecidos.
+     *
+     * @param id Identificador único da partida a ser atualizada.
+     * @param matchDto Dados atualizados da partida.
+     * @return A partida atualizada.
+     * @throws NotFoundException Caso nenhuma partida correspondente ao ID for encontrada.
+     */
     public Match replaceMatch(Long id, MatchDto matchDto) {
 
         this.findMatchById(id);

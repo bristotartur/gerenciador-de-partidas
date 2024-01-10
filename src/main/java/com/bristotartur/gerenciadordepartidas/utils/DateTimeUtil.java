a506bc3ae9117a55b25ed6691ccfea5aaac8b615
bristotartur <arturdarosabristot@gmail.com>
2024-01-10 08:56:36 -0300

    Modificações em DateTimeUtil.java adicionadas

--- src/main/java/com/bristotartur/gerenciadordepartidas/utils/DateTimeUtil.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/utils/DateTimeUtil.java
@@ -1,35 +1,24 @@
 package com.bristotartur.gerenciadordepartidas.utils;
 
-import com.bristotartur.gerenciadordepartidas.enums.ExceptionMessages;
 import com.bristotartur.gerenciadordepartidas.enums.Patterns;
-import com.bristotartur.gerenciadordepartidas.exceptions.BadRequestException;
-import lombok.experimental.UtilityClass;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-@UtilityClass
-public class DateTimeUtil {
+public final class DateTimeUtil {
+
+    private DateTimeUtil() {
+    }
 
     public static LocalDateTime parseToPattern(LocalDateTime dateTime) {
         return LocalDateTime.parse(dateTime.format(DateTimeFormatter.ofPattern(Patterns.DATE_TIME.value)));
     }
 
     public static LocalDateTime toNewMatchTime(LocalDateTime dateTime) {
-
         LocalDate date = LocalDate.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth());
-
         return LocalDateTime.of(date, LocalTime.MIN);
-    }
-
-    public static boolean checkPattern(Patterns pattern, LocalDateTime dateTime) {
-
-        if (pattern.equals(Patterns.DATE_TIME))
-            return dateTime.toString().matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}");
-
-        throw new BadRequestException(ExceptionMessages.INVALID_PATTERN.message);
     }
 
 }

7ab935fef51c53e4cc6d5ef5f7a15028c5354924
bristotartur <arturdarosabristot@gmail.com>
2024-01-11 14:55:47 -0300

    Refatoração em MatchMapper.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/mappers/MatchMapper.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/mappers/MatchMapper.java
@@ -1,49 +1,27 @@
 package com.bristotartur.gerenciadordepartidas.mappers;
 
 import com.bristotartur.gerenciadordepartidas.domain.match.structure.Match;
+import com.bristotartur.gerenciadordepartidas.domain.match.structure.MatchSport;
+import com.bristotartur.gerenciadordepartidas.domain.team.Team;
 import com.bristotartur.gerenciadordepartidas.dtos.MatchDto;
-import com.bristotartur.gerenciadordepartidas.enums.MatchStatus;
-import com.bristotartur.gerenciadordepartidas.services.GeneralMatchSportService;
-import com.bristotartur.gerenciadordepartidas.services.TeamService;
-import com.bristotartur.gerenciadordepartidas.utils.DateTimeUtil;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-@AllArgsConstructor
-public class MatchMapper {
+import java.util.List;
 
-    private final TeamService teamService;
-    private final GeneralMatchSportService generalMatchSportService;
+@Mapper(componentModel = "spring")
+public interface MatchMapper {
 
-    public Match toNewMatch(MatchDto matchDto) {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "teamScoreA", constant = "0")
+    @Mapping(target = "teamScoreB", constant = "0")
+    @Mapping(target = "modality", source = "matchDto.modality.name")
+    @Mapping(target = "matchStatus", source = "matchDto.matchStatus.name")
+    Match toNewMatch(MatchDto matchDto, MatchSport matchSport, Team teamA, Team teamB);
 
-        return Match.builder()
-                .matchSport(generalMatchSportService.newMatchSport(matchDto.sport()))
-                .teamA(teamService.findTeamById(matchDto.teamAId()))
-                .teamB(teamService.findTeamById(matchDto.teamBId()))
-                .teamScoreA(0)
-                .teamScoreB(0)
-                .modality(matchDto.modality().name)
-                .matchStatus(MatchStatus.SCHEDULED.name)
-                .matchStart(DateTimeUtil.toNewMatchTime(matchDto.matchStart()))
-                .matchEnd(DateTimeUtil.toNewMatchTime(matchDto.matchEnd()))
-                .build();
-    }
-
-    public Match toExistingMatch(Long id, MatchDto matchDto) {
-
-        return Match.builder()
-                .id(id)
-                .teamA(teamService.findTeamById(matchDto.teamAId()))
-                .teamB(teamService.findTeamById(matchDto.teamBId()))
-                .teamScoreA(matchDto.teamScoreA())
-                .teamScoreB(matchDto.teamScoreB())
-                .modality(matchDto.modality().name)
-                .matchStatus(matchDto.matchStatus().name)
-                .matchStart(matchDto.matchStart())
-                .matchEnd(matchDto.matchEnd())
-                .build();
-    }
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "modality", source = "matchDto.modality.name")
+    @Mapping(target = "matchStatus", source = "matchDto.matchStatus.name")
+    Match toExistingMatch(Long id, MatchDto matchDto, MatchSport matchSport, Team teamA, Team teamB);
 
 }

a90fa56afa3189997512e8b401e24a182bf10c7a
bristotartur <arturdarosabristot@gmail.com>
2024-01-15 14:28:50 -0300

    Classe Participant.java adicionada

---
+++ src/main/java/com/bristotartur/gerenciadordepartidas/domain/participant/Participant.java
@@ -0,0 +1,34 @@
+package com.bristotartur.gerenciadordepartidas.domain.participant;
+
+import com.bristotartur.gerenciadordepartidas.domain.team.Team;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PARTICIPANT")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String classNumber;
+
+    @JsonBackReference
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team;
+
+}

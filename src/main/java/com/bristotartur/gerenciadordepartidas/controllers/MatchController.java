53b342e7237b8d13f3471abc16ecf1e906f37cf2
bristotartur <arturdarosabristot@gmail.com>
2024-01-18 18:29:25 -0300

    Método findMatchesBySport adicionado em MatchController

--- src/main/java/com/bristotartur/gerenciadordepartidas/controllers/MatchController.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/controllers/MatchController.java
@@ -1,7 +1,9 @@
 package com.bristotartur.gerenciadordepartidas.controllers;
 
 import com.bristotartur.gerenciadordepartidas.domain.match.structure.Match;
+import com.bristotartur.gerenciadordepartidas.domain.participant.Participant;
 import com.bristotartur.gerenciadordepartidas.dtos.MatchDto;
+import com.bristotartur.gerenciadordepartidas.enums.Sports;
 import com.bristotartur.gerenciadordepartidas.services.MatchService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +35,27 @@ public class MatchController {
 
         matchList.forEach(this::addSingleMatchLink);
         return ResponseEntity.ok().body(matchList);
+    }
+
+    @GetMapping(path = "/list")
+    public ResponseEntity<List<? extends Match>> findMatchesBySport(@RequestParam Sports sport) {
+
+        List<? extends Match> matchList = matchService.findMatchesBySport(sport);
+
+        if (matchList.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        matchList.forEach(this::addSingleMatchLink);
+        return ResponseEntity.ok().body(matchList);
+    }
+
+    @GetMapping(path = "/{id}/players")
+    public ResponseEntity<List<Participant>> findAllMatchPlayers(@PathVariable Long id) {
+
+        var players = matchService.findAllMatchPlayers(id);
+
+        players.forEach(player -> addPlayerLink(player, id));
+        return ResponseEntity.ok().body(players);
     }
 
     @GetMapping(path = "/{id}")
@@ -79,16 +102,29 @@ public class MatchController {
         match.add(linkTo(methodOn(this.getClass()).findMatchById(id)).withSelfRel());
         match.add(linkTo(methodOn(TeamController.class).findTeamById(teamAId)).withRel("Team A"));
         match.add(linkTo(methodOn(TeamController.class).findTeamById(teamBId)).withRel("Team B"));
+        match.add(linkTo(methodOn(this.getClass()).findAllMatchPlayers(id)).withRel("Match players list"));
     }
 
     private void addMatchListLink(Match match) {
 
+        var id = match.getId();
         var teamAId = match.getTeamA().getId();
         var teamBId = match.getTeamB().getId();
 
         match.add(linkTo(methodOn(this.getClass()).findAllMatches()).withRel("Match list"));
         match.add(linkTo(methodOn(TeamController.class).findTeamById(teamAId)).withRel("Team A"));
         match.add(linkTo(methodOn(TeamController.class).findTeamById(teamBId)).withRel("Team B"));
+        match.add(linkTo(methodOn(this.getClass()).findAllMatchPlayers(id)).withRel("Match players list"));
+    }
+
+    private void addPlayerLink(Participant player, Long matchId) {
+
+        Long id = player.getId();
+        Long teamId = player.getTeam().getId();
+
+        player.add(linkTo(methodOn(ParticipantController.class).findParticipantById(id)).withSelfRel());
+        player.add(linkTo(methodOn(TeamController.class).findTeamById(teamId)).withRel("Team"));
+        player.add(linkTo(methodOn(this.getClass()).findMatchById(matchId)).withRel("Match"));
     }
 
 }

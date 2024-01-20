f292fc139e9e7cf7b0c8344c63f5f252f94f4246
bristotartur <arturdarosabristot@gmail.com>
2024-01-20 11:01:53 -0300

    Método findTeamMembers adicionado em TeamRepository.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/repositories/TeamRepository.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/repositories/TeamRepository.java
@@ -1,13 +1,21 @@
 package com.bristotartur.gerenciadordepartidas.repositories;
 
+import com.bristotartur.gerenciadordepartidas.domain.people.Participant;
 import com.bristotartur.gerenciadordepartidas.domain.people.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
     Optional<Team> findByName(String name);
+
+    @Query("SELECT p FROM Participant p WHERE p.team.id = :teamId")
+    List<Participant> findTeamMembers(@Param("teamId") Long teamId);
+
 }

ef7f7f4472e324f44984eb19ef38fb7de268f46e
bristotartur <arturdarosabristot@gmail.com>
2024-01-20 23:36:34 -0300

    Refatoração na classe MatchServiceTest.java

--- src/test/java/com/bristotartur/gerenciadordepartidas/services/MatchServiceTest.java
+++ src/test/java/com/bristotartur/gerenciadordepartidas/services/MatchServiceTest.java
@@ -1,14 +1,18 @@
 package com.bristotartur.gerenciadordepartidas.services;
 
 import com.bristotartur.gerenciadordepartidas.domain.people.Participant;
-import com.bristotartur.gerenciadordepartidas.domain.structure.Match;
-import com.bristotartur.gerenciadordepartidas.dtos.MatchDto;
+import com.bristotartur.gerenciadordepartidas.domain.people.Team;
 import com.bristotartur.gerenciadordepartidas.enums.Modality;
 import com.bristotartur.gerenciadordepartidas.enums.Sports;
+import com.bristotartur.gerenciadordepartidas.enums.TeamName;
 import com.bristotartur.gerenciadordepartidas.exceptions.BadRequestException;
 import com.bristotartur.gerenciadordepartidas.exceptions.NotFoundException;
 import com.bristotartur.gerenciadordepartidas.repositories.MatchRepository;
+import com.bristotartur.gerenciadordepartidas.utils.MatchTestUtil;
+import com.bristotartur.gerenciadordepartidas.utils.ParticipantTestUtil;
+import com.bristotartur.gerenciadordepartidas.utils.TeamTestUtil;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +22,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.LinkedList;
 import java.util.List;
 
-import static com.bristotartur.gerenciadordepartidas.utils.EntityTestUtil.*;
 import static com.bristotartur.gerenciadordepartidas.utils.RandomIdUtil.getRandomLongId;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @Transactional
@@ -34,42 +37,58 @@ class MatchServiceTest {
     @Autowired
     private MatchRepository matchRepository;
     @Autowired
-    private FutsalMatchService futsalMatchService;
-    @Autowired
-    private HandballMatchService handballMatchService;
+    private GeneralMatchSportService generalMatchSportService;
 
+    private Team teamA;
+    private Team teamB;
+    private Team teamC;
+    private List<Participant> players = new LinkedList<>();
+    private List<Participant> invalidPlayers = new LinkedList<>();
+
+    @BeforeEach
+    void setUp() {
+
+        teamA = TeamTestUtil.createNewTeam(TeamName.PAPA_LEGUAS, entityManager);
+        teamB = TeamTestUtil.createNewTeam(TeamName.TWISTER, entityManager);
+        teamC = TeamTestUtil.createNewTeam(TeamName.UNICONTTI, entityManager);
+
+        players.add(ParticipantTestUtil.createNewParticipant("3-53", teamA, entityManager));
+        players.add(ParticipantTestUtil.createNewParticipant("3-13", teamB, entityManager));
+
+        invalidPlayers.add(ParticipantTestUtil.createNewParticipant("3-53", teamA, entityManager));
+        invalidPlayers.add(ParticipantTestUtil.createNewParticipant("3-13", teamB, entityManager));
+        invalidPlayers.add(ParticipantTestUtil.createNewParticipant("3-81", teamC, entityManager));
+    }
 
     @Test
     @DisplayName("Should retrieve all Matches from repository when searching for all Matches")
     void Should_RetrieveAllMatchesFromRepository_When_SearchingForAllMatches() {
 
-        List<Match> existingMatches = List.of(
-                createNewMatch(Sports.FUTSAL, entityManager),
-                createNewMatch(Sports.HANDBALL, entityManager));
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players);
+        var matches = List.of(
+                generalMatchSportService.saveMatch(match, Sports.FUTSAL),
+                generalMatchSportService.saveMatch(match, Sports.HANDBALL));
 
-        existingMatches.forEach(match -> entityManager.merge(match));
+        var result = matchService.findAllMatches();
 
-        List<Match> matchList = matchService.findAllMatches();
-
-        assertEquals(existingMatches, matchList);
+        assertEquals(result, matches);
     }
 
     @Test
     @DisplayName("Should retrieve all Matches of a specific sport when specific sport is passed to search")
     void Should_RetrieveMatchesOfSpecificSport_When_SpecificSportIsPassedToSearch() {
 
-        var futsalMatchList = List.of(
-                futsalMatchService.saveMatch(createNewMatch(Sports.FUTSAL, entityManager)),
-                futsalMatchService.saveMatch(createNewMatch(Sports.FUTSAL, entityManager)));
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players);
+        var futsalMatches = List.of(
+                generalMatchSportService.saveMatch(match, Sports.FUTSAL),
+                generalMatchSportService.saveMatch(match, Sports.FUTSAL));
 
-        var genericMatchList = new LinkedList<>(
-                List.of(handballMatchService.saveMatch(createNewMatch(Sports.HANDBALL, entityManager)))
-        );
-        futsalMatchList.forEach(genericMatchList::add);
+        var genericMatchList = new LinkedList<>(List.of(generalMatchSportService.saveMatch(match, Sports.HANDBALL)));
 
+        futsalMatches.forEach(genericMatchList::add);
         var result = matchService.findMatchesBySport(Sports.FUTSAL);
 
-        assertEquals(result, futsalMatchList);
+        assertEquals(result, futsalMatches);
         assertNotEquals(result, genericMatchList);
     }
 
@@ -77,24 +96,29 @@ class MatchServiceTest {
     @DisplayName("Should find Match when existing Match ID is passed to search")
     void Should_FindMatch_When_ExistingMatchIdIsPassedToSearch() {
 
-        var existingMatch = createNewMatch(Sports.VOLLEYBALL, entityManager);
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players, Modality.MIXED);
+        var volleyballMatch = generalMatchSportService.saveMatch(match, Sports.VOLLEYBALL);
 
-        entityManager.merge(existingMatch);
+        var result = matchService.findMatchById(volleyballMatch.getId());
 
-        var existingId = existingMatch.getId();
-        var match = matchService.findMatchById(existingId);
-
-        assertEquals(match, existingMatch);
+        assertEquals(result, volleyballMatch);
     }
 
     @Test
-    @DisplayName("Should throw NotFoundException when non existing Match ID is passed to search")
-    void Should_ThrowNotFoundException_When_NonExistingMatchIdIsPassedToSearch() {
+    @DisplayName("Should throw NotFoundException when non existing Match ID is passed to any method")
+    void Should_ThrowNotFoundException_When_NonExistingMatchIdIsPassedToAnyMethod() {
 
         var id = getRandomLongId();
+        var matchDto = MatchTestUtil.createNewMatchDto(any(), any(), any(), any());
 
         assertThrows(NotFoundException.class, () -> {
             matchService.findMatchById(id);
+        });
+        assertThrows(NotFoundException.class, () -> {
+            matchService.deleteMatchById(id);
+        });
+        assertThrows(NotFoundException.class, () -> {
+            matchService.replaceMatch(id, matchDto);
         });
     }
 
@@ -102,42 +126,42 @@ class MatchServiceTest {
     @DisplayName("Should retrieve all Match players when searching for Match players")
     void Should_RetrieveAllMatchPlayers_When_SearchingForMatchPlayers() {
 
-        var match = createNewMatch(Sports.TABLE_TENNIS, entityManager);
-        entityManager.merge(match);
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players, Modality.FEMININE);
+        var handballMatch = generalMatchSportService.saveMatch(match, Sports.HANDBALL);
 
-        var players = matchService.findAllMatchPlayers(match.getId());
+        var result = matchService.findAllMatchPlayers(handballMatch.getId());
 
-        assertEquals(players, match.getPlayers());
+        assertEquals(result, handballMatch.getPlayers());
     }
 
     @Test
     @DisplayName("Should save Match when valid MatchDto is passed to save")
     void Should_SaveMatch_When_ValidMatchDtoIsPassedToSave() {
 
-        var matchDto = createNewMatchDto(Sports.TABLE_TENNIS, entityManager);
-        var savedId = matchService.saveMatch(matchDto).getId();
+        var playerIds = players.stream()
+                .map(Participant::getId)
+                .toList();
 
-        var savedMatch = matchRepository.findById(savedId).get();
+        var matchDto = MatchTestUtil.createNewMatchDto(Sports.TABLE_TENNIS, teamA.getId(), teamB.getId(), playerIds);
+        var result = matchService.saveMatch(matchDto);
 
-        assertNotNull(savedMatch);
+        assertEquals(result, matchRepository.findById(result.getId()).get());
     }
 
     @Test
     @DisplayName("Should throw BadRequestException when MatchDto with two equal team IDs is passed")
     void Should_ThrowBadRequestException_When_MatchDtoWithTwoEqualTeamIdsIsPassed() {
 
-        var existingId = futsalMatchService.saveMatch(createNewMatch(Sports.FUTSAL, entityManager)).getId();
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players);
+        var futsalMatch = generalMatchSportService.saveMatch(match, Sports.FUTSAL);
 
-        var matchDto = MatchDto.builder()
-                .teamAId(1L)
-                .teamBId(1L)
-                .build();
+        var matchDto = MatchTestUtil.createNewMatchDto(any(), 1L, 1L, any());
 
         assertThrows(BadRequestException.class, () -> {
             matchService.saveMatch(matchDto);
         });
         assertThrows(BadRequestException.class, () -> {
-            matchService.replaceMatch(existingId, matchDto);
+            matchService.replaceMatch(futsalMatch.getId(), matchDto);
         });
     }
 
@@ -145,68 +169,54 @@ class MatchServiceTest {
     @DisplayName("Should delete Match from database when Match ID is passed to delete")
     void Should_DeleteMatchFromDatabase_When_MatchIdIsPassedToDelete() {
 
-        var existingMatch = createNewMatch(Sports.CHESS, entityManager);
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players, Modality.MIXED);
+        var futsalMatch = generalMatchSportService.saveMatch(match, Sports.CHESS);
 
-        entityManager.merge(existingMatch);
+        matchService.deleteMatchById(futsalMatch.getId());
 
-        var existingId = existingMatch.getId();
-        matchService.deleteMatchById(existingId);
-
-        assertTrue(matchRepository.findById(existingId).isEmpty());
+        assertTrue(matchRepository.findById(futsalMatch.getId()).isEmpty());
     }
 
     @Test
     @DisplayName("Should update Match when MatchDto with new values is passed")
     void Should_UpdateMatch_When_MatchDtoWithNewValuesIsPassed() {
 
-        var sport = Sports.BASKETBALL;
-        var match =  matchService.saveMatch(createNewMatchDto(sport, Modality.MASCULINE, entityManager));
+        var sport = Sports.TABLE_TENNIS;
+        var originalModality = Modality.MASCULINE;
 
-        var existingId = match.getId();
-        var originalModality = match.getModality();
-        var matchDto = createNewMatchDto(sport, Modality.FEMININE, entityManager);
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players, originalModality);
+        var tableTennisMatch = generalMatchSportService.saveMatch(match, sport);
 
-        var updatedMatch = matchService.replaceMatch(existingId, matchDto);
-
-        assertNotEquals(originalModality, updatedMatch.getModality());
-    }
-
-    @Test
-    @DisplayName("Should throw NotFoundException when non existing ID is passed to replace Match")
-    void Should_ThrowNotFoundException_When_NonExistingIdIsPassedToReplaceMatch() {
-
-        var id = getRandomLongId();
-        var matchDto = createNewMatchDto(Sports.FUTSAL, entityManager);
-
-        assertThrows(NotFoundException.class, () -> {
-            matchService.replaceMatch(id, matchDto);
-        });
-    }
-
-    @Test
-    @DisplayName("Should throw BadRequestException when invalid player is passed")
-    void Should_ThrowBadRequestException_When_InvalidPlayersArePassed() {
-
-        var playerIds = createNewPlayerList(entityManager)
-                .stream()
+        var playerIds = players.stream()
                 .map(Participant::getId)
                 .toList();
 
-        var match = createNewMatch(Sports.VOLLEYBALL, entityManager);
-        entityManager.merge(match);
+        var newModality = Modality.FEMININE;
+        var matchDto = MatchTestUtil.createNewMatchDto(sport, teamA.getId(), teamB.getId(), playerIds, newModality);
+        var result = matchService.replaceMatch(tableTennisMatch.getId(), matchDto);
 
-        var matchDto = MatchDto.builder()
-                .teamAId(createNewTeam(entityManager).getId())
-                .teamBId(createNewTeam(entityManager).getId())
-                .playerIds(playerIds)
-                .sport(Sports.VOLLEYBALL)
-                .build();
+        assertNotNull(result);
+        assertNotEquals(result.getModality(), originalModality.name);
+    }
+
+    @Test
+    @DisplayName("Should throw BadRequestException when invalid players are passed to save or update Match")
+    void Should_ThrowBadRequestException_When_InvalidPlayersArePassedToSaveOrUpdateMatch() {
+
+        var playerIds = invalidPlayers.stream()
+                .map(Participant::getId)
+                .toList();
+
+        var match = MatchTestUtil.createNewMatch(teamA, teamB, players, Modality.MIXED);
+        var volleyballMatch = generalMatchSportService.saveMatch(match, Sports.VOLLEYBALL);
+
+        var matchDto = MatchTestUtil.createNewMatchDto(Sports.VOLLEYBALL, teamA.getId(), teamC.getId(), playerIds);
 
         assertThrows(BadRequestException.class, () -> {
             matchService.saveMatch(matchDto);
         });
         assertThrows(BadRequestException.class, () -> {
-            matchService.replaceMatch(match.getId(), matchDto);
+            matchService.replaceMatch(volleyballMatch.getId(), matchDto);
         });
     }
 

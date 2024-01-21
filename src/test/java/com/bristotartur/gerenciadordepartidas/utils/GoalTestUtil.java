91118feb24958636a6168f4089bca36b26feb93f
bristotartur <arturdarosabristot@gmail.com>
2024-01-20 23:37:07 -0300

    Classe utilitária GoalTestUtil.java adicionada

---
+++ src/test/java/com/bristotartur/gerenciadordepartidas/utils/GoalTestUtil.java
@@ -0,0 +1,73 @@
+package com.bristotartur.gerenciadordepartidas.utils;
+
+import com.bristotartur.gerenciadordepartidas.domain.actions.Goal;
+import com.bristotartur.gerenciadordepartidas.domain.people.Participant;
+import com.bristotartur.gerenciadordepartidas.domain.structure.Match;
+import com.bristotartur.gerenciadordepartidas.dtos.GoalDto;
+import jakarta.persistence.EntityManager;
+
+import java.time.LocalTime;
+
+/**
+ * Classe utilitária para a geração e gerenciamento de instâncias de {@link Goal} em
+ * testes de integração.
+ */
+public final class GoalTestUtil {
+
+    private GoalTestUtil() {
+    }
+
+    /**
+     * Cria uma nova instância de {@link Goal} sem persistí-la no banco de dados.
+     *
+     * @param player O jogador que marcou o gol.
+     * @param Match A partida em que o gol ocorreu.
+     * @return Uma nova instância de {@link Goal}.
+     */
+    public static Goal createNewGoal(Participant player, Match match) {
+
+        return Goal.builder()
+                .goalTime(LocalTime.now())
+                .player(player)
+                .match(match)
+                .build();
+    }
+
+    /**
+     * Cria uma nova instância de {@link Goal} e a persiste no banco de dados.
+     *
+     * @param player O jogador que marcou o gol.
+     * @param Match A partida em que o gol ocorreu.
+     * @param entityManager Instância de {@link EntityManager} responsável por gerenciar o ciclo de vida de
+     *                      entidades e persistí-las no banco de dados.
+     * @return Uma nova instância de {@link Goal}.
+     */
+    public static Goal createNewGoal(Participant player, Match match, EntityManager entityManager) {
+
+        var goal = Goal.builder()
+                .goalTime(LocalTime.now())
+                .player(player)
+                .match(match)
+                .build();
+
+        entityManager.merge(goal);
+        return goal;
+    }
+
+    /**
+     * Cria uma nova instância de {@link GoalDto}.
+     *
+     * @param playerId Identificador único do jogador que marcou o gol.
+     * @param matchId Identificador único da partida em que o gol ocorreu.
+     * @return Uma nova instância de {@link GoalDto} com os dados passados.
+     */
+    public static GoalDto createNewGoalDto(Long playerId, Long matchId) {
+
+        return GoalDto.builder()
+                .goalTime(LocalTime.now())
+                .playerId(playerId)
+                .matchId(matchId)
+                .build();
+    }
+
+}

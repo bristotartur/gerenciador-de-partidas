2081db9c4320c6b9f91e1d84de6ee1df15eea1aa
bristotartur <arturdarosabristot@gmail.com>
2024-01-22 11:42:32 -0300

    Método findMatchTypeById adicionado em MatchRepository.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/repositories/MatchRepository.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/repositories/MatchRepository.java
@@ -1,9 +1,22 @@
 package com.bristotartur.gerenciadordepartidas.repositories;
 
 import com.bristotartur.gerenciadordepartidas.domain.structure.Match;
+import jakarta.persistence.EntityManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MatchRepository<T extends Match> extends JpaRepository<T, Long> {
+
+    default String findMatchTypeById(Long id, EntityManager entityManager) {
+
+        var query = "SELECT type FROM MATCH WHERE id = :id";
+
+        return entityManager.createNativeQuery(query)
+                .setParameter("id", id)
+                .getSingleResult()
+                .toString();
+
+    }
+
 }

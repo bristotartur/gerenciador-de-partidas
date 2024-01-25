49d95a4a56400c01a1dad7f9d262cecc0f32a4ca
bristotartur <arturdarosabristot@gmail.com>
2024-01-25 16:55:47 -0300

    Logs adicionados em ParticipantController.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/controllers/ParticipantController.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/controllers/ParticipantController.java
@@ -5,6 +5,7 @@ import com.bristotartur.gerenciadordepartidas.dtos.ParticipantDto;
 import com.bristotartur.gerenciadordepartidas.services.people.ParticipantService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,12 +20,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping(value = "/gerenciador-de-partidas/api/participants")
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class ParticipantController {
 
     private final ParticipantService participantService;
 
     @GetMapping
     public ResponseEntity<List<Participant>> findAllParticipants() {
+
+        log.info("Request to find all Participants was made.");
 
         List<Participant> participantList = participantService.findAllParticipants();
 
@@ -38,23 +42,29 @@ public class ParticipantController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Participant> findParticipantById(@PathVariable Long id) {
 
-        var participant = participantService.findParticipantById(id);
+        log.info("Request to find Participant '{}' was made.", id);
 
+        var participant = participantService.findParticipantById(id);
         this.addParticipantListLink(participant);
+
         return ResponseEntity.ok().body(participant);
     }
 
     @PostMapping
     public ResponseEntity<Participant> saveParticipant(@RequestBody @Valid ParticipantDto participantDto) {
 
-        Participant participant = participantService.saveParticipant(participantDto);
+        log.info("Request to create a new Participant was made.");
 
+        Participant participant = participantService.saveParticipant(participantDto);
         this.addParticipantListLink(participant);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(participant);
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteParticipant(@PathVariable Long id) {
+
+        log.info("Request to delete Participant '{}' was made.", id);
 
         participantService.deleteParticipantById(id);
         return ResponseEntity.noContent().build();
@@ -64,9 +74,11 @@ public class ParticipantController {
     public ResponseEntity<Participant> replaceParticipant(@PathVariable Long id,
                                                           @RequestBody @Valid ParticipantDto participantDto) {
 
-        var participant = participantService.replaceParticipant(id, participantDto);
+        log.info("Request to update Participant '{}' was made.", id);
 
+        var participant = participantService.replaceParticipant(id, participantDto);
         this.addParticipantListLink(participant);
+
         return ResponseEntity.ok().body(participant);
     }
 

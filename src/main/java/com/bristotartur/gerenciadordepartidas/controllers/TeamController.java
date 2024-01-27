98b21444a52c221c56ccef0e27813a0b1576c1c3
bristotartur <arturdarosabristot@gmail.com>
2024-01-27 17:43:21 -0300

    Refatoração em TeamController.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/controllers/TeamController.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/controllers/TeamController.java
@@ -1,18 +1,21 @@
 package com.bristotartur.gerenciadordepartidas.controllers;
 
 import com.bristotartur.gerenciadordepartidas.domain.people.Team;
+import com.bristotartur.gerenciadordepartidas.dtos.ExposingTeamDto;
 import com.bristotartur.gerenciadordepartidas.dtos.TeamDto;
 import com.bristotartur.gerenciadordepartidas.enums.TeamName;
 import com.bristotartur.gerenciadordepartidas.services.people.TeamService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -27,69 +30,87 @@ public class TeamController {
     private final TeamService teamService;
 
     @GetMapping
-    public ResponseEntity<List<Team>> findAllTeams() {
+    public ResponseEntity<Page<ExposingTeamDto>> listAllTeams(Pageable pageable) {
 
-        log.info("Request to find all Teams was made.");
+        var pageNumber = pageable.getPageNumber();
+        var pageSize = pageable.getPageSize();
 
-        List<Team> teamList = teamService.findAllTeams();
-        teamList.forEach(this::addSingleTeamLink);
+        log.info("Request to get Team page of number '{}' and size '{}' was made.", pageNumber, pageSize);
 
-        return ResponseEntity.ok().body(teamList);
+        var dtoPage = this.createExposingDtoPage(teamService.findAllTeams(pageable));
+        return ResponseEntity.ok().body(dtoPage);
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Team> findTeamById(@PathVariable Long id) {
+    public ResponseEntity<ExposingTeamDto> findTeamById(@PathVariable Long id) {
 
         log.info("Request to find Team '{}' was made.", id);
 
         var team = teamService.findTeamById(id);
-        this.addTeamListLink(team);
+        var dto = this.addTeamListLink(team, PageRequest.of(0, 5));
 
-        return ResponseEntity.ok().body(team);
+        return ResponseEntity.ok().body(dto);
     }
 
     @GetMapping(path = "/find")
-    public ResponseEntity<Team> findTeamByName(@RequestParam TeamName name) {
+    public ResponseEntity<ExposingTeamDto> findTeamByName(@RequestParam TeamName name) {
 
         log.info("Request to find Team with name '{}' was made.", name);
 
         var team = teamService.findTeamByName(name);
-        this.addTeamListLink(team);
+        var dto = this.addTeamListLink(team, PageRequest.of(0, 5));
 
-        return ResponseEntity.ok().body(team);
+        return ResponseEntity.ok().body(dto);
     }
 
     @PostMapping
-    public ResponseEntity<Team> saveTeam(@RequestBody @Valid TeamDto teamDto) {
+    public ResponseEntity<ExposingTeamDto> saveTeam(@RequestBody @Valid TeamDto teamDto) {
 
         log.info("Request to create a new Team was made.");
 
         var team = teamService.saveTeam(teamDto);
-        this.addTeamListLink(team);
+        var dto = this.addTeamListLink(team, PageRequest.of(0, 5));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(team);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<Team> replaceTeam(@PathVariable Long id,
-                                            @RequestBody @Valid TeamDto teamDto) {
+    public ResponseEntity<ExposingTeamDto> replaceTeam(@PathVariable Long id,
+                                                       @RequestBody @Valid TeamDto teamDto) {
 
         log.info("Request to update Team '{}' was made.", id);
 
         var team = teamService.replaceTeam(id, teamDto);
-        this.addTeamListLink(team);
+        var dto = this.addTeamListLink(team, PageRequest.of(0, 5));
 
-        return ResponseEntity.ok().body(team);
+        return ResponseEntity.ok().body(dto);
     }
 
-    private void addSingleTeamLink(Team team) {
+    private Page<ExposingTeamDto> createExposingDtoPage(Page<Team> teamPage) {
+
+        var teams = teamPage.getContent();
+        var dtos = teams.stream()
+                .map(this::addSingleTeamLink)
+                .toList();
+
+        return new PageImpl<>(dtos, teamPage.getPageable(), teamPage.getSize());
+    }
+
+    private ExposingTeamDto addSingleTeamLink(Team team) {
 
         var id = team.getId();
-        team.add(linkTo(methodOn(this.getClass()).findTeamById(id)).withSelfRel());
+        var dto = teamService.createExposingTeamDto(team);
+
+        dto.add(linkTo(methodOn(this.getClass()).findTeamById(id)).withSelfRel());
+        return dto;
     }
 
-    private void addTeamListLink(Team team) {
-        team.add(linkTo(methodOn(this.getClass()).findAllTeams()).withRel("Team list"));
+    private ExposingTeamDto addTeamListLink(Team team, Pageable pageable) {
+
+        var dto = teamService.createExposingTeamDto(team);
+
+        dto.add(linkTo(methodOn(this.getClass()).listAllTeams(pageable)).withRel("teams"));
+        return dto;
     }
 
 }

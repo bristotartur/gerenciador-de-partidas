7e9b302a4c0b48fcbb14ff4e56f752dfa396c778
bristotartur <arturdarosabristot@gmail.com>
2024-01-28 15:58:19 -0300

    Método 'findMatchPlayers' adicionada a MatchRepository.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/repositories/MatchRepository.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/repositories/MatchRepository.java
@@ -1,9 +1,14 @@
 package com.bristotartur.gerenciadordepartidas.repositories;
 
 import com.bristotartur.gerenciadordepartidas.domain.events.Match;
+import com.bristotartur.gerenciadordepartidas.domain.people.Participant;
 import com.bristotartur.gerenciadordepartidas.enums.Sports;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -20,13 +25,22 @@ public interface MatchRepository<T extends Match> extends JpaRepository<T, Long>
      */
     default String findMatchTypeById(Long id, EntityManager entityManager) {
 
-        var query = "SELECT type FROM MATCH WHERE id = :id";
+        var query = "SELECT type FROM Match WHERE id = :id";
 
         return entityManager.createNativeQuery(query)
                 .setParameter("id", id)
                 .getSingleResult()
                 .toString();
-
     }
+
+    /**
+     * Retorna uma lista paginada de todos os jogadores associados a uma determinada instância de {@link Match}.
+     *
+     * @param id Identificador único da partida.
+     * @param pageable Um {@link Pageable} contendo informações sobre a paginação.
+     * @return Um {@link Page} contendo todos os jogadores associados a partida.
+     */
+    @Query("SELECT p FROM Participant p JOIN p.matches m WHERE m.id = :id")
+    Page<Participant> findMatchPlayers(@Param("id") Long id, Pageable pageable);
 
 }

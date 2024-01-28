2d977771e8bcf20edcc10c6b0457146aa43673d0
bristotartur <arturdarosabristot@gmail.com>
2024-01-28 17:01:53 -0300

    Método 'findMatchGoals' adicionado em GoalRepository.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/repositories/GoalRepository.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/repositories/GoalRepository.java
@@ -1,9 +1,24 @@
 package com.bristotartur.gerenciadordepartidas.repositories;
 
 import com.bristotartur.gerenciadordepartidas.domain.actions.Goal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
+
+    /**
+     * Busca por todos os gols relacionados a uma determinada partida.
+     *
+     * @param matchId Identificador único da partida.
+     * @param pageable Um {@link Pageable} contendo informações sobre a paginação.
+     * @return Um {@link Page} contendo os gols relacionados a partida especificada.
+     */
+    @Query("SELECT g FROM Goal g WHERE g.match.id = :matchId")
+    Page<Goal> findMatchGoals(@Param("matchId") Long matchId, Pageable pageable);
+
 }

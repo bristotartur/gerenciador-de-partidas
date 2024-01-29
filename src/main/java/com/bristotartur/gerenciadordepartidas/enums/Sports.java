5a80c87435ed5dbc3965f62dbc2029b05ea186e3
bristotartur <arturdarosabristot@gmail.com>
2024-01-29 14:37:51 -0300

    Método 'findSportByValue' e documentação adicionada em Sports.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/enums/Sports.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/enums/Sports.java
@@ -1,15 +1,63 @@
 package com.bristotartur.gerenciadordepartidas.enums;
 
-import lombok.AllArgsConstructor;
+import com.bristotartur.gerenciadordepartidas.exceptions.BadRequestException;
+import com.bristotartur.gerenciadordepartidas.services.events.MatchServiceFactory;
+import com.bristotartur.gerenciadordepartidas.services.events.MatchStrategy;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * Enumeração para as modalidades esportivas suportadas no sistema. Este enum pode ser usado em
+ * diversas ocasiões, como buscar partidas por um tipo específico, definir a modalidade esportiva
+ * de uma partida, filtrar o acesso de determinadas ações a eventos esportivos, etc.
+ *
+ * @see MatchStatus
+ * @see Modality
+ * @see MatchStrategy
+ * @see MatchServiceFactory
+ */
+@RequiredArgsConstructor
 public enum Sports {
+    CHESS("chess"),
+    BASKETBALL("basketball"),
     FUTSAL("futsal"),
-    HANDBALL("handebol"),
-    VOLLEYBALL("voleibol"),
-    BASKETBALL("basquetebol"),
-    TABLE_TENNIS("tênis de mesa"),
-    CHESS("xadrez");
+    HANDBALL("handball"),
+    TABLE_TENNIS("table-tennis"),
+    VOLLEYBALL("volleyball");
 
-    public final String name;
+    /**
+     * Valor interno das opções deste enum.
+     */
+    public final String value;
+
+    /**
+     * <p>Busca por uma das opções deste enum com base em seu valor interno. Os valores internos possíveis
+     * são: 'chess', 'basketball', 'futsal', 'handball', 'table-tenins' e 'volleyball'.</p> <br>
+     *
+     * Exemplo de uso:
+     * <pre>
+     *    {@code
+     *        Sports chess = Sports.findSportByValue("chess");
+     *        Sports basketball = Sports.findSportByValue("basketball");
+     *    }
+     * </pre>
+     * @param value Valor interno das opçoões deste enum.
+     * @return A opção correspondente ao valor fornecido.
+     * @throws BadRequestException Caso o valor fornecido não corresponda a nenhuma das opções do enum.
+     */
+    public static Sports findSportByValue(String value) {
+
+        Optional<Sports> sportsOptional = Arrays.stream(values())
+                .sequential()
+                .filter(sport -> sport.value.equals(value))
+                .findFirst();
+
+        if (sportsOptional.isEmpty())
+            throw new BadRequestException(ExceptionMessages.UNSUPPORTED_SPORT.message);
+
+        return sportsOptional.get();
+    }
+
 }

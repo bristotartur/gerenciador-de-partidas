f5417085ad4aa1ec517a83a4f464ed8b7e495ee0
bristotartur <arturdarosabristot@gmail.com>
2024-01-29 14:37:59 -0300

    Suíte de testes para Sports.java adicionada

---
+++ src/test/java/com/bristotartur/gerenciadordepartidas/enums/SportsTest.java
@@ -0,0 +1,31 @@
+package com.bristotartur.gerenciadordepartidas.enums;
+
+import com.bristotartur.gerenciadordepartidas.exceptions.BadRequestException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class SportsTest {
+
+    @Test
+    @DisplayName("Should find sport when valid value is passed")
+    void Should_FindSport_When_ValidValueIsPassed() {
+
+        var sportType = "futsal";
+        var result = Sports.findSportByValue(sportType);
+
+        assertEquals(result.value, sportType);
+    }
+
+    @Test
+    @DisplayName("Should throw BadRequestException when invalid value is passed")
+    void Should_Throw_BadRequestException_When_InvalidValueIsPassed() {
+        var sportType = "football";
+        assertThrows(BadRequestException.class, () -> Sports.findSportByValue(sportType));
+    }
+
+}

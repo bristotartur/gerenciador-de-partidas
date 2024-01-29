92fc81459b9d3fff5faf06345d00ee579eac8967
bristotartur <arturdarosabristot@gmail.com>
2024-01-29 17:12:06 -0300

    Método 'findMembersFromTeam' adicionado em ParticipantController.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/controllers/ParticipantController.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/controllers/ParticipantController.java
@@ -20,7 +20,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping(value = "/gerenciador-de-partidas/api/participants")
+@RequestMapping("/gerenciador-de-partidas/api/participants")
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
@@ -33,7 +33,6 @@ public class ParticipantController {
 
         var number = pageable.getPageNumber();
         var size = pageable.getPageSize();
-
         log.info("Request to get Participant page of number '{}' and size '{}' was made.", number, size);
 
         var dtoPage = this.createExposingDtoPage(participantService.findAllParticipants(pageable));
@@ -45,10 +44,20 @@ public class ParticipantController {
                                                                                    Pageable pageable) {
         var number = pageable.getPageNumber();
         var size = pageable.getPageSize();
-
         log.info("Request to get Participant page of number '{}' and size '{}' with name like '{}' was made.", number, size, name);
 
         var dtoPage = this.createExposingDtoPage(participantService.findParticipantsByNameLike(name, pageable));
+        return ResponseEntity.ok().body(dtoPage);
+    }
+
+    @GetMapping(path = "/from")
+    public ResponseEntity<Page<ExposingParticipantDto>> listMembersFromTeam(@RequestParam("team") Long teamId,
+                                                                            Pageable pageable) {
+        var number = pageable.getPageNumber();
+        var size = pageable.getPageSize();
+        log.info("Request to get Participant page of number '{}' and size '{}' from team'{}' was made.", number, size, teamId);
+
+        var dtoPage = this.createExposingDtoPage(participantService.findMambersFromTeam(teamId, pageable));
         return ResponseEntity.ok().body(dtoPage);
     }
 
@@ -58,7 +67,7 @@ public class ParticipantController {
         log.info("Request to find Participant '{}' was made.", id);
 
         var participant = participantService.findParticipantById(id);
-        var dto = this.addParticipantListLink(participant,  PageRequest.of(0, 20));
+        var dto = this.addParticipantListLink(participant, PageRequest.of(0, 20));
 
         return ResponseEntity.ok().body(dto);
     }
@@ -69,7 +78,7 @@ public class ParticipantController {
         log.info("Request to create a new Participant was made.");
 
         var participant = participantService.saveParticipant(participantDto);
-        var dto = this.addParticipantListLink(participant,  PageRequest.of(0, 20));
+        var dto = this.addParticipantListLink(participant, PageRequest.of(0, 20));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
@@ -85,7 +94,7 @@ public class ParticipantController {
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<ExposingParticipantDto> replaceParticipant(@PathVariable Long id,
-                                                          @RequestBody @Valid ParticipantDto participantDto) {
+                                                                     @RequestBody @Valid ParticipantDto participantDto) {
 
         log.info("Request to update Participant '{}' was made.", id);
 

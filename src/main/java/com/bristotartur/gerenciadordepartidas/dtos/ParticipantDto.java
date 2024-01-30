5465a7dbfafecbbc2450c5d570c9901e471e5349
bristotartur <arturdarosabristot@gmail.com>
2024-01-29 21:14:05 -0300

    Remoção de import desnecessário em ParticipantDto.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/ParticipantDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/ParticipantDto.java
@@ -1,8 +1,8 @@
 package com.bristotartur.gerenciadordepartidas.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 @Builder
 public record ParticipantDto(@NotBlank String name,

9ff19c78c6cf8d8dec3f91db270a8e5104de9c5d
bristotartur <arturdarosabristot@gmail.com>
2024-01-30 14:31:46 -0300

    DTOs para Edition.java adicionados

---
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/EditionDto.java
@@ -0,0 +1,11 @@
+package com.bristotartur.gerenciadordepartidas.dtos;
+
+import com.bristotartur.gerenciadordepartidas.enums.Status;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record EditionDto(@NotNull Status editionStatus,
+                         @NotNull LocalDate opening,
+                         @NotNull LocalDate closure) {
+}
---
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/ExposingEditionDto.java
@@ -0,0 +1,24 @@
+package com.bristotartur.gerenciadordepartidas.dtos;
+
+import com.bristotartur.gerenciadordepartidas.enums.Status;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@Getter
+public class ExposingEditionDto extends RepresentationModel<ExposingEditionDto> {
+
+    private final Long editionId;
+    private final Integer atomica;
+    private final Integer mestres;
+    private final Integer papaLeguas;
+    private final Integer twister;
+    private final Integer unicontti;
+    private final Status status;
+    private final LocalDate opening;
+    private final LocalDate closure;
+
+}

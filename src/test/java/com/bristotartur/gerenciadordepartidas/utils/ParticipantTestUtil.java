d1b18f3bc204f3f8c1dc1737205012a1c08e8065
bristotartur <arturdarosabristot@gmail.com>
2024-01-31 10:12:28 -0300

    Atualização em ParticipantTestUtil.java

--- src/test/java/com/bristotartur/gerenciadordepartidas/utils/ParticipantTestUtil.java
+++ src/test/java/com/bristotartur/gerenciadordepartidas/utils/ParticipantTestUtil.java
@@ -1,5 +1,6 @@
 package com.bristotartur.gerenciadordepartidas.utils;
 
+import com.bristotartur.gerenciadordepartidas.domain.events.Edition;
 import com.bristotartur.gerenciadordepartidas.domain.people.Participant;
 import com.bristotartur.gerenciadordepartidas.domain.people.Team;
 import com.bristotartur.gerenciadordepartidas.dtos.ParticipantDto;
@@ -19,14 +20,16 @@ public final class ParticipantTestUtil {
      *
      * @param classNumber O número da turma do participante.
      * @param team A equipe associada ao participante.
+     * @param edition A ediçõa em que o participante está inscrito.
      * @return Ua nova instância de {@link Participant}.
      */
-    public static Participant createNewParticipant(String classNumber, Team team) {
+    public static Participant createNewParticipant(String classNumber, Team team, Edition edition) {
 
         return Participant.builder()
                 .name("sa")
                 .classNumber(classNumber)
                 .team(team)
+                .edition(edition)
                 .build();
     }
 
@@ -35,16 +38,18 @@ public final class ParticipantTestUtil {
      *
      * @param classNumber O número da turma do participante.
      * @param team A equipe associada ao participante.
+     * @param edition A ediçõa em que o participante está inscrito.
      * @param entityManager Instância de {@link EntityManager} responsável por gerenciar o ciclo de vida de
      *                      entidades e persistí-las no banco de dados.
      * @return Ua nova instância de {@link Participant}.
      */
-    public static Participant createNewParticipant(String classNumber, Team team, EntityManager entityManager) {
+    public static Participant createNewParticipant(String classNumber, Team team, Edition edition, EntityManager entityManager) {
 
         var participant = Participant.builder()
                 .name("sa")
                 .classNumber(classNumber)
                 .team(team)
+                .edition(edition)
                 .build();
 
         entityManager.merge(participant);
@@ -57,16 +62,18 @@ public final class ParticipantTestUtil {
      * @param name O nome do participante.
      * @param classNumber O número da turma do participante.
      * @param team A equipe associada ao participante.
+     * @param edition A ediçõa em que o participante está inscrito.
      * @param entityManager Instância de {@link EntityManager} responsável por gerenciar o ciclo de vida de
      *                      entidades e persistí-las no banco de dados.
      * @return Ua nova instância de {@link Participant}.
      */
-    public static Participant createNewParticipant(String name, String classNumber, Team team, EntityManager entityManager) {
+    public static Participant createNewParticipant(String name, String classNumber, Team team, Edition edition, EntityManager entityManager) {
 
         var participant = Participant.builder()
                 .name(name)
                 .classNumber(classNumber)
                 .team(team)
+                .edition(edition)
                 .build();
 
         entityManager.merge(participant);
@@ -78,14 +85,16 @@ public final class ParticipantTestUtil {
      *
      * @param classNumber O número da turma do participante.
      * @param teamId Identificador único equipe associada ao participante.
+     * @param editionId Identificador único da edição na qual o participante está associado.
      * @return uma nova instância de {@link ParticipantDto}.
      */
-    public static ParticipantDto createNewParticipantDto(String classNumber, Long teamId) {
+    public static ParticipantDto createNewParticipantDto(String classNumber, Long teamId, Long editionId) {
 
         return ParticipantDto.builder()
                 .name("sa")
                 .classNumber(classNumber)
                 .teamId(teamId)
+                .editionId(editionId)
                 .build();
     }
 

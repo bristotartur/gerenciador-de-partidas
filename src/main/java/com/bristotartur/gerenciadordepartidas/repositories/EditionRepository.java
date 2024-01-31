e6cb49d202681854442ad229b29c78f2aa91d1f1
bristotartur <arturdarosabristot@gmail.com>
2024-01-30 22:24:30 -0300

    Método 'findByEditionStatus' adicionado em EditionRepository.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/repositories/EditionRepository.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/repositories/EditionRepository.java
@@ -1,9 +1,15 @@
 package com.bristotartur.gerenciadordepartidas.repositories;
 
 import com.bristotartur.gerenciadordepartidas.domain.events.Edition;
+import com.bristotartur.gerenciadordepartidas.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EditionRepository extends JpaRepository<Edition, Long> {
+
+    Optional<Edition> findByEditionStatus(Status status);
+
 }

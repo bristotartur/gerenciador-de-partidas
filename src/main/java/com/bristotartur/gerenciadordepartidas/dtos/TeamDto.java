4b47537fe0665d3c77b97767d83d7655f3331189
bristotartur <arturdarosabristot@gmail.com>
2024-01-31 16:42:44 -0300

    Remoção de classes envolvendo a entidade Team

--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/TeamDto.java
+++
@@ -1,8 +0,0 @@
-package com.bristotartur.gerenciadordepartidas.dtos;
-
-import com.bristotartur.gerenciadordepartidas.enums.TeamName;
-import jakarta.validation.constraints.NotNull;
-
-public record TeamDto(@NotNull TeamName teamName,
-                      @NotNull Integer points) {
-}

1bad98d009a0034df54c2f37e05fec84cd9397c5
bristotartur <arturdarosabristot@gmail.com>
2024-01-31 10:48:45 -0300

    Mudança na representação de campos referenteas a Team nos DTOs de Participant e Match

--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/MatchDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/MatchDto.java
@@ -3,6 +3,7 @@ package com.bristotartur.gerenciadordepartidas.dtos;
 import com.bristotartur.gerenciadordepartidas.enums.Status;
 import com.bristotartur.gerenciadordepartidas.enums.Modality;
 import com.bristotartur.gerenciadordepartidas.enums.Sports;
+import com.bristotartur.gerenciadordepartidas.enums.TeamName;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
@@ -11,8 +12,8 @@ import java.util.List;
 
 @Builder
 public record MatchDto(@NotNull Sports sport,
-                       @NotNull Long teamAId,
-                       @NotNull Long teamBId,
+                       @NotNull TeamName teamA,
+                       @NotNull TeamName teamB,
                        @NotNull List<Long> playerIds,
                        @NotNull Modality modality,
                        @NotNull Status matchStatus,
--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/ParticipantDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/ParticipantDto.java
@@ -1,5 +1,6 @@
 package com.bristotartur.gerenciadordepartidas.dtos;
 
+import com.bristotartur.gerenciadordepartidas.enums.TeamName;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -7,6 +8,6 @@ import lombok.Builder;
 @Builder
 public record ParticipantDto(@NotBlank String name,
                              @NotBlank String classNumber,
-                             @NotNull Long teamId,
+                             @NotNull TeamName team,
                              @NotNull Long editionId) {
 }

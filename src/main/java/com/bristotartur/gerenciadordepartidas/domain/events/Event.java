200d05f8e589cc5e0cdd9b14496ae23c6492ba4c
bristotartur <arturdarosabristot@gmail.com>
2024-02-01 14:10:06 -0300

    Classe abstrata Event.java adicionada a camada de domínio

---
+++ src/main/java/com/bristotartur/gerenciadordepartidas/domain/events/Event.java
@@ -0,0 +1,39 @@
+package com.bristotartur.gerenciadordepartidas.domain.events;
+
+import com.bristotartur.gerenciadordepartidas.enums.Status;
+import com.bristotartur.gerenciadordepartidas.enums.Team;
+import jakarta.persistence.*;
+import lombok.*;
+
+@MappedSuperclass
+public abstract class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Team firstPlace;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Team secondPlace;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Team thirdPlace;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Team fourthPlace;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Team fifthPlace;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status eventStatus;
+
+}

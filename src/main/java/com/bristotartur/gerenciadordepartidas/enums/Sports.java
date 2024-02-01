51d48cb38c3763a9dcf18e4f7db0345c28e19705
bristotartur <arturdarosabristot@gmail.com>
2024-02-01 18:15:58 -0300

    Implementação de EventType.java adicionada em Sports.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/enums/Sports.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/enums/Sports.java
@@ -19,7 +19,7 @@ import java.util.Optional;
  * @see MatchServiceFactory
  */
 @RequiredArgsConstructor
-public enum Sports {
+public enum Sports implements EventType {
     CHESS("chess"),
     BASKETBALL("basketball"),
     FUTSAL("futsal"),

0cf0de1503c57aee311621eb150504f59d9ce66d
bristotartur <arturdarosabristot@gmail.com>
2024-02-10 09:44:43 -0300

    Interface ExposableEventData.java adicionada ao pacote /dtos/exposing

---
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/exposing/ExposableEventData.java
@@ -0,0 +1,6 @@
+package com.bristotartur.gerenciadordepartidas.dtos.exposing;
+
+import com.bristotartur.gerenciadordepartidas.domain.events.Event;
+
+public interface ExposableEventData<T extends Event> {
+}

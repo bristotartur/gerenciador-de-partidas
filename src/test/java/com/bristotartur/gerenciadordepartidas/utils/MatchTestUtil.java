c09ce0132639eada57cb7e4fcfe5ba7a74578b9e
bristotartur <arturdarosabristot@gmail.com>
2024-02-11 16:08:41 -0300

    Atualização em MatchTestUtil.java

--- src/test/java/com/bristotartur/gerenciadordepartidas/utils/MatchTestUtil.java
+++ src/test/java/com/bristotartur/gerenciadordepartidas/utils/MatchTestUtil.java
@@ -4,10 +4,7 @@ import com.bristotartur.gerenciadordepartidas.domain.events.SportEvent;
 import com.bristotartur.gerenciadordepartidas.domain.people.Participant;
 import com.bristotartur.gerenciadordepartidas.domain.matches.Match;
 import com.bristotartur.gerenciadordepartidas.dtos.input.MatchDto;
-import com.bristotartur.gerenciadordepartidas.enums.Status;
-import com.bristotartur.gerenciadordepartidas.enums.Modality;
-import com.bristotartur.gerenciadordepartidas.enums.Sports;
-import com.bristotartur.gerenciadordepartidas.enums.Team;
+import com.bristotartur.gerenciadordepartidas.enums.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,6 +29,7 @@ public final class MatchTestUtil {
     public static Match createNewMatch(Team teamA, Team teamB, List<Participant> players, SportEvent event) {
 
         return Match.builder()
+                .matchImportance(Importance.NONE)
                 .teamA(teamA)
                 .teamB(teamB)
                 .players(players)
@@ -57,6 +55,7 @@ public final class MatchTestUtil {
     public static Match createNewMatch(Team teamA, Team teamB, List<Participant> players, SportEvent event, Modality modality) {
 
         return Match.builder()
+                .matchImportance(Importance.NONE)
                 .teamA(teamA)
                 .teamB(teamB)
                 .players(players)
@@ -82,6 +81,7 @@ public final class MatchTestUtil {
     public static Match createNewMatch(Team teamA, Team teamB, List<Participant> players, SportEvent event, Status status) {
 
         return Match.builder()
+                .matchImportance(Importance.NONE)
                 .teamA(teamA)
                 .teamB(teamB)
                 .players(players)
@@ -90,6 +90,32 @@ public final class MatchTestUtil {
                 .teamScoreB(0)
                 .modality(Modality.MASCULINE)
                 .matchStatus(status)
+                .matchStart(LocalDateTime.now())
+                .matchEnd(LocalDateTime.now())
+                .build();
+    }
+
+    /**
+     * Cria uma nova instância de {@link Match} sem persistí-la no banco de dados.
+     *
+     * @param teamA Constante de {@link Team} que representa a equipe A da partida.
+     * @param teamB Constante de {@link Team} que representa a equipe B da partida.
+     * @param players Os jogadores presentes na partida.
+     * @param importance O peso da partida, podendo ser normal, semifinal, disputa pelo terceiro lugar ou final.
+     * @return Uma nova instância de {@link Match} com os dados fornecidos.
+     */
+    public static Match createNewMatch(Team teamA, Team teamB, List<Participant> players, SportEvent event, Importance importance) {
+
+        return Match.builder()
+                .matchImportance(importance)
+                .teamA(teamA)
+                .teamB(teamB)
+                .players(players)
+                .event(event)
+                .teamScoreA(0)
+                .teamScoreB(0)
+                .modality(Modality.MASCULINE)
+                .matchStatus(Status.SCHEDULED)
                 .matchStart(LocalDateTime.now())
                 .matchEnd(LocalDateTime.now())
                 .build();
@@ -107,6 +133,7 @@ public final class MatchTestUtil {
     public static MatchDto createNewMatchDto(Sports sport, Team teamA, Team teamB, List<Long> playerIds, Long eventId) {
 
         return MatchDto.builder()
+                .matchImportance(Importance.NONE)
                 .sport(sport)
                 .teamA(teamA)
                 .teamB(teamB)
@@ -132,6 +159,7 @@ public final class MatchTestUtil {
     public static MatchDto createNewMatchDto(Sports sport, Team teamA, Team teamB, List<Long> playerIds, Long eventId, Modality modality) {
 
         return MatchDto.builder()
+                .matchImportance(Importance.NONE)
                 .sport(sport)
                 .teamA(teamA)
                 .teamB(teamB)
@@ -157,6 +185,7 @@ public final class MatchTestUtil {
     public static MatchDto createNewMatchDto(Sports sport, Team teamA, Team teamB, List<Long> playerIds, Long eventId, Status status) {
 
         return MatchDto.builder()
+                .matchImportance(Importance.NONE)
                 .sport(sport)
                 .teamA(teamA)
                 .teamB(teamB)

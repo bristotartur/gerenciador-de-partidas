ea4cf4599e836b71c59df59c67e0ac6d5d4bf76e
bristotartur <arturdarosabristot@gmail.com>
2024-02-18 21:39:08 -0300

    Campo 'editionStatus' removido de EditionDto.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/input/EditionDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/input/EditionDto.java
@@ -1,11 +1,9 @@
 package com.bristotartur.gerenciadordepartidas.dtos.input;
 
-import com.bristotartur.gerenciadordepartidas.enums.Status;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
-public record EditionDto(@NotNull Status editionStatus,
-                         @NotNull LocalDate opening,
+public record EditionDto(@NotNull LocalDate opening,
                          @NotNull LocalDate closure) {
 }

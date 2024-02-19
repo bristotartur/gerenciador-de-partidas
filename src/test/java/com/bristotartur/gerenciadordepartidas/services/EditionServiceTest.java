68b2a4f87cf9b06e22699f536f9d659f5e686467
bristotartur <arturdarosabristot@gmail.com>
2024-02-18 22:14:38 -0300

    Novos testes adicionado na suíte EditionServiceTest.java
    Testes para os métodos 'updateEditionStatus' e 'checkEditionStatusById' foram adicionados, além de ajustes nos testes de 'saveEdition' e 'updateEdition' que não possuem mais a função de checar o Status das edições.

--- src/test/java/com/bristotartur/gerenciadordepartidas/services/EditionServiceTest.java
+++ src/test/java/com/bristotartur/gerenciadordepartidas/services/EditionServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import static com.bristotartur.gerenciadordepartidas.utils.RandomIdUtil.getRandomLongId;
@@ -69,34 +70,22 @@ class EditionServiceTest {
     void Should_ThrowNotFoundException_When_InvalidEditionIdisPassedToAnyMethod() {
 
         var id = getRandomLongId();
-        var dto = new EditionDto(any(), any(), any());
+        var dto = new EditionDto(any(), any());
 
         assertThrows(NotFoundException.class, () -> editionService.findEditionById(id));
         assertThrows(NotFoundException.class, () -> editionService.deleteEditionById(id));
         assertThrows(NotFoundException.class, () -> editionService.replaceEdition(id, dto));
+        assertThrows(NotFoundException.class, () -> editionService.updateEditionStatus(id, any()));
     }
 
     @Test
     @DisplayName("Should save Edition when valid EditionDto is passed to save")
     void Should_SaveEdition_When_ValidEditionDtoIsPassedToSave() {
 
-        var dto = new EditionDto(Status.SCHEDULED, LocalDate.now(), LocalDate.now());
+        var dto = new EditionDto(LocalDate.now(), LocalDate.now());
         var result = editionService.saveEdition(dto);
 
         assertTrue(editionRepository.findById(result.getId()).isPresent());
-    }
-
-    @Test
-    @DisplayName("Should throw BadRequestException when trying to create an Edition with accept status")
-    void Should_ThrowBadRequestException_When_TriyngToCreateEditionWithNonAccesptStatus() {
-
-        var dtoA = new EditionDto(Status.IN_PROGRESS, LocalDate.now(), LocalDate.now());
-        var dtoB = new EditionDto(Status.ENDED, LocalDate.now(), LocalDate.now());
-        var dtoC = new EditionDto(Status.OPEN_FOR_EDITS, LocalDate.now(), LocalDate.now());
-
-        assertThrows(BadRequestException.class, () -> editionService.saveEdition(dtoA));
-        assertThrows(BadRequestException.class, () -> editionService.saveEdition(dtoB));
-        assertThrows(BadRequestException.class, () -> editionService.saveEdition(dtoC));
     }
 
     @Test
@@ -127,11 +116,33 @@ class EditionServiceTest {
     void Should_UpdateEdition_When_EditionDtoWithNewValuesIsPassed() {
 
         var edition = EditionTestUtil.createNewEdition(Status.SCHEDULED, entityManager);
-        var dto = new EditionDto(Status.IN_PROGRESS, LocalDate.now(), LocalDate.now());
+        var dto = new EditionDto(LocalDate.of(2024, Month.FEBRUARY, 16), LocalDate.of(2024, Month.FEBRUARY, 17));
 
         var result = editionService.replaceEdition(edition.getId(), dto);
 
         assertNotEquals(result, edition);
+    }
+
+    @Test
+    @DisplayName("Should not throw anything when valid Status is passed to update Edition Status")
+    void Should_NotThrowAnything_When_ValidStatusIsPassedToUpdateEditionStatus() {
+
+        var edition = EditionTestUtil.createNewEdition(Status.SCHEDULED, entityManager);
+
+        assertDoesNotThrow(() -> editionService.updateEditionStatus(edition.getId(), Status.IN_PROGRESS));
+        assertDoesNotThrow(() -> editionService.updateEditionStatus(edition.getId(), Status.ENDED));
+        assertDoesNotThrow(() -> editionService.updateEditionStatus(edition.getId(), Status.OPEN_FOR_EDITS));
+        assertDoesNotThrow(() -> editionService.updateEditionStatus(edition.getId(), Status.ENDED));
+    }
+
+    @Test
+    @DisplayName("Should not throw BadRequestException when invalid Status is passed to update Edition Status")
+    void Should_ThrowBadRequestException_When_InvalidStatusIsPassedToUpdateEditionStatus() {
+
+        var edition = EditionTestUtil.createNewEdition(Status.SCHEDULED, entityManager);
+
+        assertThrows(BadRequestException.class, () -> editionService.updateEditionStatus(edition.getId(), Status.ENDED));
+        assertThrows(BadRequestException.class, () -> editionService.updateEditionStatus(edition.getId(), Status.OPEN_FOR_EDITS));
     }
 
     @Test
@@ -140,9 +151,28 @@ class EditionServiceTest {
 
         var editionA = EditionTestUtil.createNewEdition(Status.IN_PROGRESS, entityManager);
         var editionB = EditionTestUtil.createNewEdition(Status.SCHEDULED, entityManager);
-        var dto = new EditionDto(Status.IN_PROGRESS, LocalDate.now(), LocalDate.now());
 
-        assertThrows(BadRequestException.class, () -> editionService.replaceEdition(editionB.getId(), dto));
+        assertThrows(BadRequestException.class, () -> editionService.updateEditionStatus(editionB.getId(), Status.IN_PROGRESS));
+    }
+
+    @Test
+    @DisplayName("Should not throw anything when trying to operate on unfinished Editions")
+    void Should_NotThrowAnything_When_TryingToOperateOnUnfinishedEditions() {
+
+        var editionA = EditionTestUtil.createNewEdition(Status.SCHEDULED, entityManager);
+        var editionB = EditionTestUtil.createNewEdition(Status.IN_PROGRESS, entityManager);
+        var editionC = EditionTestUtil.createNewEdition(Status.OPEN_FOR_EDITS, entityManager);
+
+        assertDoesNotThrow(() -> editionService.checkEditionStatusById(editionA.getId()));
+        assertDoesNotThrow(() -> editionService.checkEditionStatusById(editionB.getId()));
+        assertDoesNotThrow(() -> editionService.checkEditionStatusById(editionC.getId()));
+    }
+
+    @Test
+    @DisplayName("Should throw BadRequestException when trying to operate on unfinished Editions")
+    void Should_ThrowBadRequestException_When_TryingToOperateOnFinishedEditions() {
+        var edition = EditionTestUtil.createNewEdition(Status.ENDED, entityManager);
+        assertThrows(BadRequestException.class, () -> editionService.checkEditionStatusById(edition.getId()));
     }
 
 }

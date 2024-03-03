db5ae581b86662c943efb2e5dae3f8d1287ad7fe
bristotartur <arturdarosabristot@gmail.com>
2024-03-02 22:30:35 -0300

    Métodos 'handleConflictException' e 'handleForbiddenException' adicionados a RestExceptionHandler.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/handlers/RestExceptionHandler.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/handlers/RestExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.bristotartur.gerenciadordepartidas.handlers;
 
-import com.bristotartur.gerenciadordepartidas.exceptions.BadRequestException;
-import com.bristotartur.gerenciadordepartidas.exceptions.NotFoundException;
-import com.bristotartur.gerenciadordepartidas.exceptions.ExceptionDetails;
+import com.bristotartur.gerenciadordepartidas.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,26 +12,51 @@ import java.time.LocalDateTime;
 public class RestExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ExceptionDetails> handleNotFoundException(NotFoundException notFoundException) {
+    public ResponseEntity<ExceptionDetails> handleNotFoundException(NotFoundException exception) {
 
         return new ResponseEntity<>(ExceptionDetails.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.NOT_FOUND.value())
                 .title("NotFoundException.")
-                .details(notFoundException.getMessage())
-                .developerMessage(notFoundException.getClass().getName())
+                .details(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
                 .build(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ExceptionDetails> handleBadRequestException(BadRequestException badRequestException) {
+    public ResponseEntity<ExceptionDetails> handleBadRequestException(BadRequestException exception) {
 
         return new ResponseEntity<>(ExceptionDetails.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .title("BadRequestException.")
-                .details(badRequestException.getMessage())
-                .developerMessage(badRequestException.getClass().getName())
+                .details(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
                 .build(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ExceptionDetails> handleConflictException(ConflictException exception) {
+
+        return new ResponseEntity<>(ExceptionDetails.builder().
+                timestamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .title("ConflictException.")
+                .details(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
+                .build(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ExceptionDetails> handleForbiddenException(ForbiddenException exception) {
+
+        return new ResponseEntity<>(ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.FORBIDDEN.value())
+                .title("ForbiddenException")
+                .details(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
+                .build(), HttpStatus.FORBIDDEN);
+    }
+
 }

9c461caef29f691b18d193b95f291c7afe267354
bristotartur <arturdarosabristot@gmail.com>
2024-03-11 22:46:42 -0300

    Método 'handleHttpMessageNotReadableException' adicionado em RestExceptionHandler.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/handlers/RestExceptionHandler.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/handlers/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package com.bristotartur.gerenciadordepartidas.handlers;
 import com.bristotartur.gerenciadordepartidas.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -59,6 +60,18 @@ public class RestExceptionHandler {
                 .developerMessage(exception.getClass().getName())
                 .fields(fields)
                 .fieldsMessages(fieldsMessages)
+                .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionDetails> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+
+        return new ResponseEntity<>(ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Bad Request.")
+                .details(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
                 .build(), HttpStatus.BAD_REQUEST);
     }
 

ded57c05058eb563aac8ea5189c217f4362dbc30
bristotartur <arturdarosabristot@gmail.com>
2024-03-13 21:35:17 -0300

    Anotações 'Schema' adicionadas em EditionDto.java e SportEventDto.java

--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/input/EditionDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/input/EditionDto.java
@@ -1,9 +1,16 @@
 package com.bristotartur.gerenciadordepartidas.dtos.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
-public record EditionDto(@NotNull LocalDate opening,
-                         @NotNull LocalDate closure) {
+@Schema(description = "DTO de requisição de Edições")
+public record EditionDto(
+
+        @Schema(description = "Data de início da da Edição", example = "2024-04-01")
+        @NotNull LocalDate opening,
+
+        @Schema(description = "Data de encerramento da Edição", example = "2024-05-15")
+        @NotNull LocalDate closure) {
 }
--- src/main/java/com/bristotartur/gerenciadordepartidas/dtos/input/SportEventDto.java
+++ src/main/java/com/bristotartur/gerenciadordepartidas/dtos/input/SportEventDto.java
@@ -3,12 +3,25 @@ package com.bristotartur.gerenciadordepartidas.dtos.input;
 import com.bristotartur.gerenciadordepartidas.domain.events.SportEvent;
 import com.bristotartur.gerenciadordepartidas.enums.Modality;
 import com.bristotartur.gerenciadordepartidas.enums.Sports;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record SportEventDto(@NotNull Sports type,
-                            @NotNull Modality modality,
-                            @NotNull Integer totalMatches,
-                            @NotNull Long editionId) implements TransferableEventData<SportEvent> {
+@Schema(description = "DTO de requisição de Eventos Esportivos")
+public record SportEventDto(
+
+        @Schema(description = "Tipo de esporte do Evento Esportivo", example = "FUTSAL")
+        @NotNull Sports type,
+
+        @Schema(description = "Modalidade do Evento Esportivo", example = "MASCULINE")
+        @NotNull Modality modality,
+
+        @Schema(description = "Total de partidas do Evento Esportivo", example = "14")
+        @NotNull Integer totalMatches,
+
+        @Schema(description = "Identificador único da Edição do Evento Esportivo", example = "1")
+        @NotNull Long editionId
+
+) implements TransferableEventData<SportEvent> {
 }
